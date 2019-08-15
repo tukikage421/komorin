88b10ca91b72b843a442d70e8f5fb0a5ad4acbe0
tukikage421 <54142359+tukikage421@users.noreply.github.com>
2019-08-15 22:49:13 +0900

    Create discordbot.py

---
+++ discordbot.py
@@ -0,0 +1,26 @@
+import discord
+
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = 'NjExNTI5NTc3MzQ4NzI2Nzg1.XVVO_A.zfr-tpagQUk32i7ISeUS_EVc1jI'
+
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
+
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
